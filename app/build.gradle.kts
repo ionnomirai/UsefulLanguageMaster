c629bcec898f7c74cef927af407b474785a5f8ad
ionnomirai <kindbakaneko@gmail.com>
2025-04-03 03:44:21 -0700

    Created a layout for the preview fragment
    - added Binding for layouts;
    - created a layout for the "fr_preview" fragment;
    - for "fr_preview" I added an image for the background and a logo. Also added a font, string resources and colors;
    - added libraries for fragments and navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Navigation
+    val nav_version = "2.8.9"
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    // Fragmentds and Navigation
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
 }
